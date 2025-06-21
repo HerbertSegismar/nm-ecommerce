690bbf95a9d4a946047648381d9bea5e6e5466fd
Herb Segismar <herbert.segismar@yahoo.com>
2025-06-22 07:17:26 +1200

    fd /

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -2,6 +2,8 @@ import Container from '@/components/Container'
 import HomeBanner from '@/components/HomeBanner'
 import React from 'react'
 
+export const dynamic = "force-dynamic"; // Disables prerendering
+
 const Home = () => {
   return (
     <Container className='bg-lightBrown rounded-md'>
