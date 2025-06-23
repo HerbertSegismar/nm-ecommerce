628c0d8b7931496987e916be1c937f449f081b29
Herb Segismar <herbert.segismar@yahoo.com>
2025-06-23 23:09:59 +1200

    disable auth

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,32 +5,33 @@ import Navbar from './Navbar'
 import SearchBar from './SearchBar'
 import CartIcon from './CartIcon'
 import FavButton from './FavButton'
-import Login from './Login'
 import MobileMenu from './MobileMenu'
-import { currentUser } from '@clerk/nextjs/server'
-import { ClerkLoaded, SignedIn, UserButton } from '@clerk/nextjs'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
-const Header = async() => {
-  const user = await currentUser()
+const Header = () => {
 
   return (
     <header className="bg-white py-5 border-b-black/20 border-b">
       <Container className="flex items-center justify-between">
         <div className="flex items-center justify-center gap-2 md:gap-0">
           <MobileMenu />
-          <Logo className="hover:scale-105"/>
+          <Logo className="hover:scale-105" />
         </div>
         <Navbar />
         <div className="flex items-center gap-4 text-darkGreen">
           <SearchBar />
           <CartIcon />
           <FavButton />
-          <ClerkLoaded>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {/* {!user && <Login />} */}
+          {/* <ClerkLoaded>
             <SignedIn>
               <UserButton />
             </SignedIn>
             {!user && <Login />}
-          </ClerkLoaded>
+          </ClerkLoaded> */}
         </div>
       </Container>
     </header>
