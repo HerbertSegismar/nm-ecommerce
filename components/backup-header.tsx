4b6d35444ff87d48374a664089ff26ba74da2823
Herb Segismar <herbert.segismar@yahoo.com>
2025-06-28 22:14:00 +1200

    disable middleware

--- components/backup-header.tsx
+++ components/backup-header.tsx
@@ -7,22 +7,30 @@ import CartIcon from './CartIcon'
 import FavButton from './FavButton'
 import Login from './Login'
 import MobileMenu from './MobileMenu'
+import { currentUser } from '@clerk/nextjs/server'
+import { ClerkLoaded, SignedIn, UserButton } from '@clerk/nextjs'
 
-const Header = () => {
+const Header = async() => {
+  const user = await currentUser()
 
   return (
     <header className="bg-white py-5 border-b-black/20 border-b">
       <Container className="flex items-center justify-between">
         <div className="flex items-center justify-center gap-2 md:gap-0">
           <MobileMenu />
-          <Logo className="hover:scale-105" />
+          <Logo className="hover:scale-105"/>
         </div>
         <Navbar />
         <div className="flex items-center gap-4 text-darkGreen">
           <SearchBar />
           <CartIcon />
           <FavButton />
-          <Login />
+          <ClerkLoaded>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            {!user && <Login />}
+          </ClerkLoaded>
         </div>
       </Container>
     </header>
