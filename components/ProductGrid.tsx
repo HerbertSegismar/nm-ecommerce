1a5256f70eca795332aa2a63b0c4faeab90a6862
Herb Segismar <herbert.segismar@yahoo.com>
2025-06-29 18:32:04 +1200

    producttypes

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,11 +1,8 @@
 'use client'
-import { useState } from 'react'
 import HometabBar from './HometabBar'
 
 const ProductGrid = () => {
-    const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [selectedTab, setSelectedTab] = useState("")
+    
   return (
     <div className='py-10'>
         <HometabBar />
