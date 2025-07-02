3e724d1d9036093abd2d9e7f311794654b8b7d02
Herb Segismar <herbert.segismar@yahoo.com>
2025-07-02 23:21:23 +1200

    No products available loading screen

--- components/HometabBar.tsx
+++ components/HometabBar.tsx
@@ -1,21 +1,35 @@
 import { productType } from "@/constants/NavData";
+import Link from "next/link";
 import React from "react";
 
-const HometabBar = () => {
+interface Props {
+  selectedTab: string;  
+  onTabSelect: (tab: string) => void;
+}
+
+const HometabBar = ({ selectedTab, onTabSelect }: Props) => {
   return (
     <div className="flex items-center justify-between flex-wrap gap-5">
-      <div className="text-sm font-semibold flex items-center gap-3">
+      <div className="text-sm font-semibold flex flex-wrap items-center gap-3">
         {productType?.map((item) => (
           <button
+            onClick={() => onTabSelect(item?.name)}
             key={item?.name}
             className={`border border-lightGreen/30 px-4 py-1.5 hover:border-lightGreen
-                    md:px-6 md:py-2 rounded-full hover:bg-lightGreen hover:text-white hoverEffect`}
+                    md:px-6 md:py-2 rounded-full hover:bg-lightGreen hover:text-white hoverEffect
+                    ${selectedTab === item?.name? "bg-lightGreen border-lightGreen text-white" : "bg-lightGreen/10"}`}
           >
             {item?.name}
           </button>
         ))}
       </div>
-      <div>See All</div>
+      <Link
+        href="/shop"
+        className="border border-lightGreen/30 px-4 py-1.5 hover:border-lightGreen text-sm
+                    md:px-6 md:py-2 rounded-full hover:bg-lightGreen hover:text-white hoverEffect"
+      >
+        See All
+      </Link>
     </div>
   );
 };
--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,11 +1,54 @@
 'use client'
+import { useEffect, useState } from 'react'
 import HometabBar from './HometabBar'
+import { productType } from '@/constants/NavData'
+import { client } from '@/sanity/lib/client'
+import { motion } from 'motion/react'
+import { Loader2 } from 'lucide-react'
+import NoProductsAvailable from './NoProductsAvailable'
 
 const ProductGrid = () => {
-    
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [selectedTab, setSelectedTab] = useState(productType[0].name || "")
+
+    const query = `*[_type == "product" && variant == $variant] | order(name desc){
+    ..., "categories": categories[]->name }`;
+
+    const params = {
+        variant: selectedTab.toLowerCase()
+    }
+
+    useEffect(()=> {
+      const fetchdata = async () => {
+        setLoading(true)
+        try {
+          const response = await client.fetch(query, params) 
+          setProducts(response) 
+        } catch (error) {
+          console.error("Product fetch error:", error)
+        } finally {
+          setLoading(false)
+        }
+      }
+      fetchdata()
+    }, [selectedTab])
+
   return (
     <div className='py-10'>
-        <HometabBar />
+        <HometabBar selectedTab={selectedTab} onTabSelect={setSelectedTab}/>
+        {loading? (
+          <div className='flex flex-col items-center justify-center py-10 min-h-80 gap-4 bg-gray-100 w-full mt-10'>
+            <div className='space-x-2 flex items-center text-blue-600'>
+              <Loader2 className='w-5 h-6 animate-spin'/>
+              <span>Loading Products</span>
+            </div> 
+          </div>
+        ) : products?.length? (
+          <>Products</>
+        ) : (
+          <NoProductsAvailable selectedTab={selectedTab}/>
+        )}
     </div>
   )
 }
